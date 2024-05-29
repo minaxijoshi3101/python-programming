1810ac6ec0798ceb3abc8b76bc4815563a416071
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-05-29 22:14:38 +0800

    changes

--- hello.py
+++ hello.py
@@ -1,4 +1,22 @@
-import pandas
-import hashlib
+print ("Hello, world")
+print (8+8)
+print("******your poem, here******")
+'''
+Please dont remove this line
+Author: Minaxi Joshi
+100days of code
+'''
+"""
+Please dont remove this line
+Author: Minaxi Joshi
+100days of code
+"""
 
-print("Hello")
+print("I am good girl \nand Amita is a good girl too") #printing a multiline stmt.
+print("Hey, I am a \"good girl\"\nshe is a \"good girl too")
+print ("I am \"good and\n intellegent\" ")
+#seperator
+print("Hey",6,7,sep="###")
+print("Hi",end="-------")
+print("again")
+print("once again")
