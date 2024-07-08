55882702cad75177889185cb8c772e8604ff5d61
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2024-07-08 11:50:13 +0800

    exception handling and file

--- exception-handling/exp.py
+++ exception-handling/exp.py
@@ -8,10 +8,17 @@ class TooOldException(Exception):
     def __init__(self,msg):
         self.msg=msg
 
-age=int(input("enter age "))
-if age>60 :
-    raise TooYoungException('Please wait..')
-elif age < 14 :
-    raise TooOldException('you have alr crossed your age..')
-else:
-    print("you will get matched details soon")
+try: 
+    age=int(input("enter age "))
+    if age>60 :
+        raise TooYoungException('Please wait..')
+    elif age < 14 :
+        raise TooOldException('you have alr crossed your age..')
+    else:
+        print("you will get matched details soon")
+except TooYoungException as e:
+    print(f"TooYoungException: {e.msg}")
+except TooOldException as e:
+    print(f"TooOldException: {e.msg}")
+
+print("end of code")
--- hello.py
+++ hello.py
@@ -12,16 +12,6 @@ Author: Minaxi Joshi
 100days of code
 """
 
-<<<<<<< HEAD
-print("I am good girl \nand Amita is a good girl too") #printing a multiline stmt.
-print("Hey, I am a \"good girl\"\nshe is a \"good girl too")
-print ("I am \"good and\n intellegent\" ")
-#seperator
-print("Hey",6,7,sep="###")
-print("Hi",end="-------")
-print("again")
-print("once again")
-=======
 a = complex(6,2)
 e = complex(8,2)
 #text data- string
@@ -45,4 +35,3 @@ print(18*6)
 print(18/6)
 print(18//6)
 print(18%6)
->>>>>>> 5149718c3f0f54f6fde6f7561edfb3779b63336e
