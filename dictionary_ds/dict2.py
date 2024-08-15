6e3db69b634c1204073c73dd5258dfedfe73d74b
minaxi <minaxijoshi3101@gmail.com>
2024-08-15 15:57:01 +0800

    dictionary practice

--- dictionary_ds/dict2.py
+++ dictionary_ds/dict2.py
@@ -3,4 +3,9 @@ while True:
     name=input("enter name of the student ")
     marks=input("enyer marks of the student ")
     d[name]=marks
-    option=input("Do you wanna continue ")
+    option=input("Do you wanna continue? enter [yes|no]")
+    if(option.lower()=="no"):
+        break
+    elif(option.lower()!="yes" | option.loweer()!="no"):
+        print("wrong entry. enter [yes|no]")
+print(d)
