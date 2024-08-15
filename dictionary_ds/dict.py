9a8e8505d918f1bf60afd03888f438cdea4875c9
minaxi <minaxijoshi3101@gmail.com>
2024-08-15 12:38:05 +0800

    dictionary practice

---
+++ dictionary_ds/dict.py
@@ -0,0 +1,16 @@
+d={} #or
+d1=dict()
+print(type(d))
+d[100]="minaxi"
+d[200]="sumit"
+d[300]="amita"
+print(d)
+print(type(d[100]))
+print(type(d1))
+print(d[100])
+key=300
+if(key in d):
+    print(d[key])
+else:
+    print("key is not available")
+#invalid --> d2={1,2,3 : 'a','b','c'}
---
+++ dictionary_ds/dict2.py
@@ -0,0 +1,6 @@
+d={}
+while True:
+    name=input("enter name of the student ")
+    marks=input("enyer marks of the student ")
+    d[name]=marks
+    option=input("Do you wanna continue ")
