d3f234cb76af9fdcd766633963b15752bd06b1e9
unknown <minaxijoshi3101@gmail.com>
2024-10-21 22:30:04 +0800

    immutsbility

---
+++ day10/immutability.py
@@ -0,0 +1,12 @@
+a=258
+b=258
+print(id(a))
+print(id(b))
+print(id(a)==id(b))
+print(a is b)
+
+x=10.0
+y=10.0
+print(x is y)
+
+p1=10+20j
--- day10/type_casting.py
+++ day10/type_casting.py
@@ -1,5 +1,32 @@
-str='10'
-print(int(str))
+str1='10'
+print(int(str1))
 
 i=19.5
 print(int(i))
+
+print(float(0x7564))
+
+#print(int(1+2j))
+
+print(int(True))
+
+print(int('10'))
+
+print(float(True))
+
+print(complex(False))
+
+#print(complex(10,'20'))
+
+#print(complex('10',3))
+
+print(complex(10,10.4))
+
+#string to bool
+print(bool('True'))
+print(bool('False'))
+print(bool('Yes'))
+print(bool(' '))
+#str
+print(str(10))
+print(str(0B1111))
