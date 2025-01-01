82eaa520b0a85d32ee5d9eb300cdf1d030b32f57
unknown <minaxijoshi3101@gmail.com>
2025-01-01 21:33:42 +0800

    class based view implemented in django and data will be fethced

--- connect_django.py
+++ connect_django.py
@@ -1,6 +1,7 @@
 import requests
 BASE_URL='http://127.0.0.1:8000/' 
-ENDPOINT='cities_json/'
+#ENDPOINT='cities_json/'
+ENDPOINT="cities_json_cbv/"
 response=requests.get(BASE_URL+ENDPOINT)
 print(response.json())
 print(type(response.json()))
