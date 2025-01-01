86bae93698e2f4329bb9255e43565e4252e622c1
unknown <minaxijoshi3101@gmail.com>
2025-01-01 22:10:14 +0800

    post request sent for testing

--- connect_django.py
+++ connect_django.py
@@ -1,7 +1,8 @@
 import requests
 BASE_URL='http://127.0.0.1:8000/' 
 #ENDPOINT='cities_json/'
-ENDPOINT="cities_json_cbv/"
+#ENDPOINT="cities_json_cbv/"
+ENDPOINT='cities_json_cbv2/'
 response=requests.get(BASE_URL+ENDPOINT)
 print(response.json())
 print(type(response.json()))
@@ -11,3 +12,7 @@ print('*'*50)
 print('City ID:',data['id'])
 print('City Name:',data['city_name'])
 print('State Name:',data['state_name'])
+print('*'*50)
+response2=requests.post(BASE_URL+ENDPOINT)
+print(type(response2.json()))
+print(response2.json())
